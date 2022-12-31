da909b2306ab7ac26c57ee98bf8c0eedaeae31f4
“rmooing <zzzzlucky@naver.com>
2022-12-31 21:30:09 +0900

    스크롤 버튼 색상 변경

---
+++ ridibooks_clonecording/js/index.js
@@ -0,0 +1,7 @@
+// 맨 위로 올라가는 버튼
+
+const scrollBtn  = document.querySelector(".up_btn");
+
+scrollBtn.addEventListener("click", function() {
+  window.scrollTo(0,0);
+});
--- ridibooks_clonecording/js/select.js
+++ ridibooks_clonecording/js/select.js
@@ -1,8 +1,23 @@
-const closeBtn = document.querySelector("close_btn");
-const popUp = document.querySelector("popup");
+//메인 배너 버튼 
+const main = document.querySelector(".main_img");
+const bannerImg = document.querySelector(".banner");
+const mainBtn = document.querySelector(".main_btn");
+let targetX = 0;
 
-function popUp() {
-  closeBtn.style.display = "none";
+for(i = 0; i < )
+
+//메인 베스트 배너 버튼
+
+
+
+//팝업 닫기 버튼
+const closeBtn = document.querySelector(".close_btn");
+const popUp = document.querySelector(".popup");
+
+function popUpClose() {
+  popUp.classList.remove("popup");
 }
 
-closeBtn.addEventListener("click", popUp)
+closeBtn.addEventListener("click", popUpClose);
+
+
