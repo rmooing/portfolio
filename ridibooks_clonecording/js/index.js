29874678bb6d3aed8c47cb5313cad308b7d30aa6
“rmooing <zzzzlucky@naver.com>
2023-01-01 13:33:46 +0900

    소스수정

--- ridibooks_clonecording/js/index.js
+++ ridibooks_clonecording/js/index.js
@@ -3,5 +3,8 @@
 const scrollBtn  = document.querySelector(".up_btn");
 
 scrollBtn.addEventListener("click", function() {
+  // window.scrollTo(0,0).scrollIntoView({behavior: "smooth"});
   window.scrollTo(0,0);
+  window.scrollIntoView({block:"start", behavior:'smooth'});
+  // window.scrollIntoView({behavior:"smooth"});
 });
--- ridibooks_clonecording/js/select.js
+++ ridibooks_clonecording/js/select.js
@@ -1,10 +1,18 @@
 //메인 배너 버튼 
-const main = document.querySelector(".main_img");
+const mainSec = document.querySelector(".main_sec")
+const mainImg = document.querySelector(".main_img");
 const bannerImg = document.querySelector(".banner");
-const mainBtn = document.querySelector(".main_btn");
-let targetX = 0;
 
-for(i = 0; i < )
+const leftBtn = document.querySelector(".left_btn");
+const rightBtn = document.querySelector(".right_btn");
+
+let bgImg = document.querySelector(".bg_img");
+let imgCount = bgImg.length;
+let currentIndex = 0;
+
+lefttBtn.addEventListener("click", function(){
+
+});
 
 //메인 베스트 배너 버튼
 
@@ -16,6 +24,7 @@ const popUp = document.querySelector(".popup");
 
 function popUpClose() {
   popUp.classList.remove("popup");
+
 }
 
 closeBtn.addEventListener("click", popUpClose);
